8a9c4ae8c9952048b02ea127a7c7ca80c03afc60
alexander.korotkov <manderlai87@gmail.com>
2017-02-01 17:11:37 +0200

    update gitignore

--- app/signUp/sign-up-fields.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var SignUpFields = (function () {
-    function SignUpFields() {
-    }
-    return SignUpFields;
-}());
-exports.SignUpFields = SignUpFields;
-//# sourceMappingURL=sign-up-fields.js.map
