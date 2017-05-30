f5e294e1df8ff80239de117c67931e41f88360b6
alexanderKorotkov <manderlai87@gmai.com>
2017-05-30 16:51:14 +0300

    heroku update

---
+++ Server.js
@@ -0,0 +1,11 @@
+/**
+ * Created by alexander on 30.05.17.
+ */
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
